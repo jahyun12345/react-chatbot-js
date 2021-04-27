7f4d3ed70dd759ba3c1f2f8abb0b2b6e52d35800
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-27 17:24:17 +0900

    * card component setting

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -2,10 +2,13 @@ import React, { useEffect } from 'react';
 import Axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { saveMessage } from '../_actions/message_actions';
+import Message from './Sections/Message';
 
 function Chatbot() {
     // trigger the action
     const dispatch = useDispatch();
+    // state.message.messages : redux-state
+    const messagesFromRedux = useSelector(state => state.message.messages);
 
     // 입력 없이 반환되는 값이기 때문에 useEffect에서 eventQuery 호출
     useEffect(() => {
@@ -15,7 +18,7 @@ function Chatbot() {
     const textQuery = async (text) => {
         // need to take care of the message client sent
         let conversation = {
-            who: 'client',
+            who: 'user',
             content: {
                 text: {
                     text: text
@@ -98,10 +101,25 @@ function Chatbot() {
         }
     }
 
+    const renderOneMessage = (message, index) => {
+        // console.log(message);
+        return <Message key={index} who={message.who} text={message.content.text.text} />
+    }
+
+    const renderMessage = (returnedMessages) => {
+        if (returnedMessages) {
+            return returnedMessages.map((message, index) => {
+                return renderOneMessage(message, index);
+            })
+        } else {
+            return null; 
+        }
+    }
+
     return (
         <div style={{height:700, width:700, border:'3px solid black', borderRadius:'7px'}}>
             <div style={{height:644, width:'100%', overflow:'auto'}}>
-
+                {renderMessage(messagesFromRedux)}
             </div>
             <input
                 style={{margin:0, width:'100%', height:50,
--- client/src/Chatbot/Sections/Message.js
+++ client/src/Chatbot/Sections/Message.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { List, Icon, Avatar } from 'antd';
 
 function Message(props) {
-
-    const AvatarSrc = props.who ==='bot' ? <Icon type="robot" /> : <Icon type="smile" />  
+    const AvatarSrc = props.who === 'bot' ? <Icon type="robot" /> : <Icon type="smile" />;
 
     return (
-        <List.Item style={{ padding: '1rem' }}>
+        <List.Item style={{padding:'1rem'}}>
             <List.Item.Meta
                 avatar={<Avatar icon={AvatarSrc} />}
                 title={props.who}
