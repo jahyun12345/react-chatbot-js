8ea25536167e97998ae252660bb0d292a695ea69
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-27 15:13:11 +0900

    * event query route setting

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -16,7 +16,6 @@ const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 // text query route
 router.post('/textQuery', async (req, res) => {
     // need to send some information that comes from the client to Dialogflow API
-
     // The text query request.
     const request = {
         session: sessionPath,
@@ -36,13 +35,34 @@ router.post('/textQuery', async (req, res) => {
     const result = responses[0].queryResult;
     console.log(`  Query: ${result.queryText}`);
     console.log(`  Response: ${result.fulfillmentText}`);
+
     res.send(result);
 })
 
 // event query route
+router.post('/eventQuery', async (req, res) => {
+  // need to send some information that comes from the client to Dialogflow API
+  // The text query request.
+  const request = {
+      session: sessionPath,
+      queryInput: {
+        event: {
+          // The query to send to the dialogflow agent
+          name: req.body.event,
+          // The language used by the client (en-US)
+          languageCode: languageCode,
+        },
+      },
+  };
+   
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
 
-
-
-
+  res.send(result);
+})
 
 module.exports = router;
