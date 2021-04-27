c0f3a7c2fc9665da238273a8ca1c6d1c2c177b62
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-27 15:31:17 +0900

    * text query function setting at client

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,13 +1,57 @@
 import React from 'react';
+import Axios from 'axios';
 
 function Chatbot() {
+    const textQuery = async (text) => {
+        // let conversations = [];
+
+        // need to take care of the message client sent
+        let conversation = {
+            who: 'user',
+            content: {
+                text: {
+                    text: text
+                }
+            }
+        }
+        // conversations.push(conversation);
+        // need to take care of the message chatbot sent
+        const textQueryVariables = {
+            text
+        }
+        
+        try {
+            // send request to text query route
+            // text query route에서 async await 사용하였으므로 설정 필요
+            const response = await Axios.post('/api/dialogflow/textQuery', textQueryVariables);
+            const content = response.data.fulfillmentMessages[0];
+            conversation = {
+                who: 'bot',
+                content: content
+            }
+            console.log({tryConversation:conversation});
+            // conversations.push(conversation);
+        } catch (error) {
+            conversation = {
+                who: 'bot',
+                content: {
+                    text: {
+                        text: "Error just occured, please check the problem."
+                    }
+                }
+            }
+            console.log({catchConversation:conversation});
+            // conversations.push(conversation);
+        }
+    }
+
     const keyPressHandler = (e) => {
         if (e.key === "Enter") {
             if (!e.target.value) {
                 return alert('you need to type something first!');
             }
             // send request to text query route
-            // textQuery(e.target.value);
+            textQuery(e.target.value);
             e.target.value = "";
         }
     }
